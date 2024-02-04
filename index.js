b1625119e9b61c037fdaafa72c472f03c797a87b
Zoin Onu <ziononwujuba233@gmail.com>
2024-02-04 13:02:25 -0600

    stuffs

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ require("dotenv").config(); //for using variables from .env file.
 const express = require("express");
 const mongoose = require("mongoose");
 const JournalModel = require("./models/journal")
+
 const app = express();
-var session = require('express-session');
 const port = 3000;
 //const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/";
 const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/?retryWrites=true&w=majority";
-var passport = require('passport');
-var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+
 
 
 mongoose.connect(process.env.MONGODB_URL || uri).then(() => {
@@ -20,55 +19,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 app.set('views', './views');
 app.set("view engine", "ejs")
-
-app.use(session({ secret: 'keyboard cat' }));
-app.use(passport.initialize());
-app.use(passport.session({
-  resave: false,
-  saveUninitialized: true
-}));
-var fitbitStrategy = new FitbitStrategy({
-  clientID: "23RTQD",
-  clientSecret: "fce8b10c985c39fac31229e8a5ae5973",
-  scope: ['activity','heartrate','location','profile'],
-  code_challenge_method: "S256",
-  callbackURL: "https://tsamentalhealthapp-0fee6615a9d9.herokuapp.com/auth/fitbit/callback",
-  response_type: "code",
-  code_challenge: "mefRFf3vcN_EAxVhYgWNBS6LDFVAu6dlHh__IEd-6u8",
-}, function(accessToken, refreshToken, profile, done) {
-  // TODO: save accessToken here for later use
-
-  done(null, {
-    accessToken: accessToken,
-    refreshToken: refreshToken,
-    profile: profile
-  });
-
-});
-
-//app.use(passport.authenticate('session'));
-passport.use(fitbitStrategy);
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-var fitbitAuthenticate = passport.authenticate('fitbit', {
-  successRedirect: '/auth/fitbit/success',
-  failureRedirect: '/auth/fitbit/failure'
-});
-
-app.get('/auth/fitbit', fitbitAuthenticate);
-app.get('/auth/fitbit/callback', fitbitAuthenticate);
-
-app.get('/auth/fitbit/success', function(req, res, next) {
-  res.send(req.user);
-});
-
 
 
 //Shows newly created journal entry
