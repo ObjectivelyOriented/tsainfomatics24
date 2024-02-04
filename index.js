4ebdaf6da277da3c19adc27cfa1bbc290de6d0f8
Zoin Onu <ziononwujuba233@gmail.com>
2024-02-04 08:32:08 -0600

    Fitbit API Test

--- index.js
+++ index.js
@@ -3,9 +3,55 @@ const express = require("express");
 const mongoose = require("mongoose");
 const JournalModel = require("./models/journal")
 const app = express();
+var session = require('express-session');
 const port = 3000;
 //const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/";
-const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/?retryWrites=true&w=majority";
+var passport = require('passport');
+var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+
+var fitbitStrategy = new FitbitStrategy({
+  clientID: "23RTQD",
+  clientSecret: "fce8b10c985c39fac31229e8a5ae5973",
+  scope: ['activity','heartrate','location','profile'],
+  callbackURL: "http://localhost:3000/auth/fitbit/callback"
+}, function(accessToken, refreshToken, profile, done) {
+  // TODO: save accessToken here for later use
+
+  done(null, {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+    profile: profile
+  });
+
+});
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.authenticate('session'));
+passport.use(fitbitStrategy);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var fitbitAuthenticate = passport.authenticate('fitbit', {
+  successRedirect: '/auth/fitbit/success',
+  failureRedirect: '/auth/fitbit/failure'
+});
+
+app.get('/auth/fitbit', fitbitAuthenticate);
+app.get('/auth/fitbit/callback', fitbitAuthenticate);
+
+app.get('/auth/fitbit/success', function(req, res, next) {
+  res.send(req.user);
+});
 
 mongoose.connect(process.env.MONGODB_URL || uri).then(() => {
   console.log("MongoDB is connected!");
@@ -16,6 +62,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 app.set('views', './views');
 app.set("view engine", "ejs")
+app.use(passport.initialize());
+app.use(passport.session({
+  resave: false,
+  saveUninitialized: true
+}));
 
 
 //Shows newly created journal entry
