e81db5d30db14e165613afbcd79b936446cedca4
Zoin Onu <ziononwujuba233@gmail.com>
2024-02-05 23:02:32 -0600

    Added basic fitbit api calls, no frontend work

--- index.js
+++ index.js
@@ -5,22 +5,6 @@ const JournalModel = require("./models/journal")
 var axios = require("axios").default;
 const app = express();
 const port = 3000;
-//const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/";
-const uri = "mongodb+srv://HendricksonTSA:a4MzaUFZ67HcIFdh@tsa2324.nwxsyzn.mongodb.net/?retryWrites=true&w=majority";
-//Put client id and secret in env file
-
-var authOptions = {
-  method: 'POST',
-  url: 'https://api.fitbit.com/oauth2/token',
-  headers: {'content-type': 'application/x-www-form-urlencoded', Authorization: "Basic " + Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET, 'utf-8').toString('base64')},
-  data: new URLSearchParams({
-    grant_type: 'authorization_code',
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    code: '',
-    redirect_uri: 'https://tsamentalhealthapp-0fee6615a9d9.herokuapp.com/callback'
-  })
-};
 
 var apiCallOptions = {
   method: 'GET',
@@ -80,24 +64,36 @@ app.get("/journals", async (req, res) => {
   }
 });
 app.get("/callback", function (req, res) {
+  
+var authOptions = {
+  method: 'POST',
+  url: 'https://api.fitbit.com/oauth2/token',
+  headers: {'content-type': 'application/x-www-form-urlencoded', Authorization: "Basic " + Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET, 'utf-8').toString('base64')},
+  data: new URLSearchParams({
+    grant_type: 'authorization_code',
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: '',
+    redirect_uri: 'https://tsamentalhealthapp-0fee6615a9d9.herokuapp.com/callback'
+  })
+};
+var testAuthOptions = {
+  method: 'POST',
+  url: 'https://api.fitbit.com/oauth2/token',
+  headers: {'content-type': 'application/x-www-form-urlencoded', Authorization: "Basic " + Buffer.from(process.env.TEST_CLIENT_ID + ":" + process.env.TEST_CLIENT_SECRET, 'utf-8').toString('base64')},
+  data: new URLSearchParams({
+    grant_type: 'authorization_code',
+    client_id: process.env.TEST_CLIENT_ID,
+    client_secret: process.env.TEST_CLIENT_SECRET,
+    code: req.query.code,
+    redirect_uri: 'https://arcane-castle-84229-a0015ab2dc2b.herokuapp.com/callback',
+    code_verifier: '4w2y554p2d5f3u323b1k3m032w6v442g3610362j2o354l713y116d1k3q1n5f6i04004a3y2f2k4k2h4p5215434z3r361j0q4s474z1n5x1x716w4221631q2g6s5y'
+  })
+};
   console.log(req.query);
   //TODO: Add if statement to check if state in url is equal to generated state
   //Access token request
   //axios.request(authOptions).then(function (response) {
-    var testAuthOptions = {
-      method: 'POST',
-      url: 'https://api.fitbit.com/oauth2/token',
-      headers: {'content-type': 'application/x-www-form-urlencoded', Authorization: "Basic " + Buffer.from(process.env.TEST_CLIENT_ID + ":" + process.env.TEST_CLIENT_SECRET, 'utf-8').toString('base64')},
-      data: new URLSearchParams({
-        grant_type: 'authorization_code',
-        client_id: process.env.TEST_CLIENT_ID,
-        client_secret: process.env.TEST_CLIENT_SECRET,
-        code: req.query.code,
-        redirect_uri: 'https://arcane-castle-84229-a0015ab2dc2b.herokuapp.com/callback',
-        code_verifier: '4w2y554p2d5f3u323b1k3m032w6v442g3610362j2o354l713y116d1k3q1n5f6i04004a3y2f2k4k2h4p5215434z3r361j0q4s474z1n5x1x716w4221631q2g6s5y'
-      })
-    };
-    console.log(testAuthOptions.data);
     
     axios.request(testAuthOptions).then(function (response) {
     console.log(response.data);
@@ -105,16 +101,19 @@ app.get("/callback", function (req, res) {
     testApiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
     //API call
    // axios.request(apiCallOptions).then(function (response) {
-    axios.request(testApiCallOptions).then(function (response) {
-      console.log(response.data);
-      res.status(201).json(response.data);
-    }).catch(function (error) {
-      console.error("API call error" + error);
-    });
+    
   }).catch(function (error) {
     console.error("Token request error " + error);
   });
 
+});
+app.get("/request", function (req, res) {
+axios.request(testApiCallOptions).then(function (response) {
+  console.log(response.data);
+  res.status(201).json(response.data);
+}).catch(function (error) {
+  console.error("API call error" + error);
+});
 });
 
 //Shows home page
