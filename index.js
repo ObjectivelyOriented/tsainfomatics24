bf1cf9c98c10aef544dd8af1861df5432f276ce4
Zoin Onu <ziononwujuba233@gmail.com>
2024-02-25 19:34:05 -0600

    Test api

--- index.js
+++ index.js
@@ -156,8 +156,8 @@ var testAuthOptions = {
     
     //axios.request(testAuthOptions).then(function (response) {
     console.log(response.data);
-    apiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
-    //testApiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
+    //apiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
+    testApiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
     //API call
     axios.request(testAuthOptions).then(function (response) {
       console.log(response.data);
@@ -175,13 +175,13 @@ var testAuthOptions = {
 //test request after fitbit auth
 app.get("/request", function (req, res) {
 //axios.request(testApiCallOptions).then(function (response) {
-   axios.request(apiCallOptions).then(function (response) {
+   //axios.request(apiCallOptions).then(function (response) {
   console.log(response.data);
   res.status(201).json(response.data);
 }).catch(function (error) {
   console.error("API call error" + error);
 });
-});
+//});
 
 
 //TODO: add fitbit refresh token route
