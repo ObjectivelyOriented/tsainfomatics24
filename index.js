50e713f39d977004359cde773d1ebed31bc1710a
Zoin Onu <ziononwujuba233@gmail.com>
2024-02-25 21:42:01 -0600

    Test api

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var apiCallOptions = {
 
 var testApiCallOptions = {
   method: 'GET',
-  url: 'https://api.fitbit.com/1/user/-/profile.json',
+  url: '',
   headers: {'content-type': 'application/json', Authorization: ''}
 };
 
@@ -87,6 +87,11 @@ app.post("/api/journal", async (req, res)=>{
 })
 app.get("/fitbit", async (req, res) => {
 
+  res.redirect("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23RTQD&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+sleep+social+temperature+weight&code_challenge="+ code_challenge +"&code_challenge_method=S256&redirect_uri=https%3A%2F%2Ftsamentalhealthapp-0fee6615a9d9.herokuapp.com%2Fcallback");
+  
+});
+app.get("/testfitbit", async (req, res) => {
+
   res.redirect("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23RVHM&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight&code_challenge="+ code_challenge +"&code_challenge_method=S256&redirect_uri=https%3A%2F%2Farcane-castle-84229-a0015ab2dc2b.herokuapp.com%2Fcallback");
   
 });
@@ -132,36 +137,21 @@ var authOptions = {
     client_secret: "fce8b10c985c39fac31229e8a5ae5973",
     code: req.query.code,
     redirect_uri: 'https://tsamentalhealthapp-0fee6615a9d9.herokuapp.com/callback',
-    code_verifier: "2u61543l3b2g3x60601n5c553348732v3t07475x3j2k6g2y5u4l6p6z130e1u3m47504f1d2l3g3h6g2e5q2m6n670h1x6v16345j101h4q0x4x2q015l1k0j2r6d3s"
-  })
-};
-var testAuthOptions = {
-  method: 'POST',
-  url: 'https://api.fitbit.com/oauth2/token',
-
-  headers: {'content-type': 'application/x-www-form-urlencoded', Authorization: "Basic " + Buffer.from("23RVHM" + ":" + "db95c38a5330ceadb41e0e0e333630ff", 'utf-8').toString('base64')},
-  data: new URLSearchParams({
-    grant_type: 'authorization_code',
-    client_id: "23RVHM",
-    client_secret: "db95c38a5330ceadb41e0e0e333630ff",
-   
-    code: req.query.code,
-    redirect_uri: 'https://arcane-castle-84229-a0015ab2dc2b.herokuapp.com/callback',
     code_verifier: code_verifier
   })
 };
+
   console.log(req.params);
   console.log(req.query.code);
   //TODO: Add if statement to check if state in url is equal to generated state
   //Access token request
-  axios.request(testAuthOptions).then(function (response) {
+  axios.request(authOptions).then(function (response) {
     
-    //axios.request(testAuthOptions).then(function (response) {
     console.log(response.data);
-    //apiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
-    testApiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
+    apiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
+    
     //API call
-    axios.request(testApiCallOptions).then(function (response) {
+    axios.request(apiCallOptions).then(function (response) {
       console.log(response.data);
       res.status(201).json(response.data);
     }).catch(function (error) {
@@ -173,6 +163,52 @@ var testAuthOptions = {
   });
 
 });
+
+app.get("/testcallback", function (req, res) {
+  
+
+  var testAuthOptions = {
+    method: 'POST',
+    url: 'https://api.fitbit.com/oauth2/token',
+  
+    headers: {'content-type': 'application/x-www-form-urlencoded', Authorization: "Basic " + Buffer.from("23RVHM" + ":" + "db95c38a5330ceadb41e0e0e333630ff", 'utf-8').toString('base64')},
+    data: new URLSearchParams({
+      grant_type: 'authorization_code',
+      client_id: "23RVHM",
+      client_secret: "db95c38a5330ceadb41e0e0e333630ff",
+      code: req.query.code,
+      redirect_uri: 'https://arcane-castle-84229-a0015ab2dc2b.herokuapp.com/testcallback',
+      code_verifier: code_verifier
+    })
+  };
+    console.log(req.params);
+    console.log(req.query.code);
+    //TODO: Add if statement to check if state in url is equal to generated state
+    //Access token request
+    axios.request(testAuthOptions).then(function (response) {
+      
+      //axios.request(testAuthOptions).then(function (response) {
+      console.log(response.data);
+      //apiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
+      res.redirect("/");
+     // res.redirect('/')
+    }).catch(function (error) {
+      console.error("Token request error " + error);
+    });
+  
+  });
+  app.get("/fitbit/profile", function(req,res){
+    testApiCallOptions.url = "https://api.fitbit.com/1/user/-/profile.json";
+    testApiCallOptions.headers.Authorization = "Bearer " + response.data.access_token;
+      //API call
+      axios.request(testApiCallOptions).then(function (response) {
+        console.log(response.data);
+        res.status(201).json(response.data);
+      }).catch(function (error) {
+        console.error("API call error" + error);
+      });
+
+  });
 
 //test request after fitbit auth
 /*
