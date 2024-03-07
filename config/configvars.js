442d34a158dbe99e8a8e200fd2e76458129fda91
Zoin Onu <ziononwujuba233@gmail.com>
2024-03-06 18:01:40 -0600

    indexjs cleanup

--- config/configvars.js
+++ config/configvars.js
@@ -3,8 +3,3 @@ const randomstring = require("randomstring");
 const code_verifier = randomstring.generate(128);
 const authCode = '';
 
-
-module.exports.authOptions = authOptions;
-module.exports.authCode = authCode;
-module.exports.code_verifier = code_verifier;
-module.exports.testAuthOptions = testAuthOptions;
