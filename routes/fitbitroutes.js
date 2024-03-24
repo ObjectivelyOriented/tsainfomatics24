b9724000a9895a44cad2a0f713e2fb26d079c5b0
Zoin Onu <ziononwujuba233@gmail.com>
2024-03-24 16:15:55 -0500

    Added journal stuff

--- routes/fitbitroutes.js
+++ routes/fitbitroutes.js
@@ -172,7 +172,7 @@ var apiCallOptions = {
       
         //API call
         axios.request(apiCallOptions).then(function (response) {
-          console.log(response.data["activities-heart"]);
+          console.log(response.data["activities-heart"].heartRateZones[0]);
           res.status(201).json(response.data);
         }).catch(function (error) {
           console.error("API call error" + error);
