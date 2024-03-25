b0a770b920a751d8ee87ce8383d4c9f4bb1105a1
Zoin Onu <ziononwujuba233@gmail.com>
2024-03-24 21:41:50 -0500

    fitbit graphs

--- routes/fitbitroutes.js
+++ routes/fitbitroutes.js
@@ -169,10 +169,12 @@ var apiCallOptions = {
   
     });
     router.get("/heart", function(req,res){
-      apiCallOptions.url = "https://api.fitbit.com/1/user/-/activities/heart/date/2024-02-28/1d/1min.json";
-      apiCallOptions.headers.Authorization = "Bearer " + (req.user.fitbitData.accessToken);
+      //apiCallOptions.url = "https://api.fitbit.com/1/user/-/activities/heart/date/2024-02-28/1d/1min.json";
+      //apiCallOptions.headers.Authorization = "Bearer " + (req.user.fitbitData.accessToken);
         //API call
-        axios.request(apiCallOptions).then(function (response) {
+        var heartRate = [99, 98, 97, 96, 95];
+        res.json(heartRate);
+       /* axios.request(apiCallOptions).then(function (response) {
           var heartLabels = [];
           var heartRate = [];
           for(zone of response.data["activities-heart"][0].value.heartRateZones){
@@ -184,7 +186,7 @@ var apiCallOptions = {
         }).catch(function (error) {
           console.error("API call error" + error);
           res.status(401).redirect("/fitbit/refreshTokens");
-        });
+        });*/
   
     });
 
