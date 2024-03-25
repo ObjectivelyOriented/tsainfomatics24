c1ea3cf11bc85a7a0d763575db3b7f3d748a00be
Zoin Onu <ziononwujuba233@gmail.com>
2024-03-24 22:09:04 -0500

    fitbit graphs

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,6 +2,7 @@ import * as d3 from "https://cdn.skypack.dev/d3@7";
 import axios from 'https://cdn.skypack.dev/axios';
 
 const dataSet = async function getData() {
+    console.log("Here");
     return await axios.get('/fitbit/heart');
 }
 async function drawChart() {
